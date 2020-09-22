c8a31a9db26b7a92924ab3d6cab09a98c01e03fe
Uffe Hansen <uffe.hansen@gmail.com>
2020-09-22 17:39:57 +0200

    After docker talk

--- dockerwcfservice/dockerwcfservice/Program.cs
+++ dockerwcfservice/dockerwcfservice/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,30 @@ namespace dockerwcfservice
     {
         static void Main(string[] args)
         {
+            Uri baseAddress = new Uri("http://localhost:8242/dockerwcfservice");
+
+            // Create the ServiceHost.
+            using (ServiceHost host = new ServiceHost(typeof(Dockerwcfservice),baseAddress))
+            {
+                // Enable metadata publishing.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                host.Description.Behaviors.Add(smb);
+
+                // Open the ServiceHost to start listening for messages. Since
+                // no endpoints are explicitly configured, the runtime will create
+                // one endpoint per base address for each service contract implemented
+                // by the service.
+                host.Open();
+
+                Console.WriteLine("The service is ready at {0}", baseAddress);
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+
+                // Close the ServiceHost.
+                host.Close();
+            }
         }
     }
 }
